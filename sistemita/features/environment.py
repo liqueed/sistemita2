1a4ea6eec496e130bfd3e33aaca68640c64e281f
Alan Cyment <acyment@gmail.com>
2020-03-13 18:47:30 +0000

    Fixture para pruebas de UI con Selenium

--- sistemita/features/environment.py
+++ sistemita/features/environment.py
@@ -1,2 +1,18 @@
+from behave import fixture, use_fixture
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
 def before_all(context):
     context.fixtures = ['datos_iniciales.yaml']
+
+@fixture
+def browser(context, timeout=30, **kwargs):
+    options = Options()
+    options.headless = True
+    context.browser = webdriver.Firefox(options=options)
+    context.add_cleanup(context.browser.quit)
+    return context.browser
+
+def before_tag(context, tag):
+    if tag == "fixture.browser":
+        use_fixture(browser, context, timeout=10)
