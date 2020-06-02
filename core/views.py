4273edabf3d81863a94f1e178108068cd5c61b07
Daniel Padula <hola@danielpadula.com>
2020-06-02 17:32:32 -0300

    Base de usuarios

--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import TemplateView, ListView
+
+from authorization.models import User
 
 
 class ClienteListView(LoginRequiredMixin, TemplateView):
     template_name = 'core/home.html'
+
+
+class UsuarioListView(LoginRequiredMixin, ListView):
+    model = User
+    template_name = 'core/user_list.html'
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
--- sistemita/urls.py
+++ sistemita/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from core.views import HomeView, ClienteListView
+from core.views import HomeView, ClienteListView, UsuarioListView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', include('facturacion_clientes.urls'))
     path('accounts/', include('django.contrib.auth.urls')),
     path('', HomeView.as_view(), name='home'),
-    path('cliente/', ClienteListView.as_view(), name='cliente-list')
+    path('clientes/', ClienteListView.as_view(), name='cliente-list'),
+    path('usuarios/', UsuarioListView.as_view(), name='usuario-list')
 ]
