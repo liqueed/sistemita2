432164c6d7d953d0c028b42966ea46bcd78ae45c
Marisol Cardozo <cardozomarisolp@gmail.com>
2020-11-30 17:53:01 -0300

    Agrega endpoint para agregar cobranzas y refactoriza template

--- accounting/views/cobranzas.py
+++ accounting/views/cobranzas.py
@@ -5,8 +5,38 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.views.generic import ListView, TemplateView
 
+# Django Rest Framework
+from rest_framework import permissions
+from rest_framework import mixins, status
+from rest_framework import viewsets
+from rest_framework.response import Response
+
 # Models
 from accounting.models.cobranza import Cobranza
+
+# Serializers
+from accounting.serializers import (
+    AddCobranzaSerializer, CobranzaSerializer
+)
+
+
+class CobranzaViewSet(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.ListModelMixin,
+                      viewsets.GenericViewSet):
+    """Cobranza view set."""
+    serializer_class = CobranzaSerializer
+    queryset = Cobranza.objects.all()
+    permission_classes = (permissions.AllowAny,)  # TODO: Only test
+
+    def create(self, request, *args, **kwargs):
+        """Handle member creation from invitation code."""
+        serializer = AddCobranzaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        cobranza = serializer.save()
+
+        data = self.get_serializer(cobranza).data
+        return Response(data, status=status.HTTP_201_CREATED)
 
 
 class CobranzaListView(LoginRequiredMixin, ListView):
