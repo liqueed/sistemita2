d24282d682cf14e7b4e5c03c3840c54b5430db86
Marisol Cardozo <cardozomarisolp@gmail.com>
2022-12-19 18:40:08 -0300

    Agrega configuraciones de email

--- config/settings/base.py
+++ config/settings/base.py
@@ -244,11 +244,6 @@ CORS_URLS_REGEX = r"^/api/.*$"
 
 # EMAIL
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND",
-    default="django.core.mail.backends.smtp.EmailBackend",
-)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
 EMAIL_FROM = 'ivy@liqueed.org'
--- config/settings/production.py
+++ config/settings/production.py
@@ -119,3 +119,16 @@ ignore_logger("django.security.DisallowedHost")
 # SITE
 # ------------------------------------------------------------------------------
 SITE_URL = 'https://sistemita.liqueed.org'
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = env(
+    "DJANGO_EMAIL_BACKEND",
+    default="django.core.mail.backends.smtp.EmailBackend",
+)
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
